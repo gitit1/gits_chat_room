09ba44ada53ae17d3ee28934709fef9d28967dec
git <gitit1@msn.com>
2020-10-20 13:43:47 +0300

    Update ChatRoom.js
    Fix connection to work in both localhost and on server

--- client/src/containers/ChatRoom/ChatRoom.js
+++ client/src/containers/ChatRoom/ChatRoom.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import Chat from './components/Chat';
 import NewMessage from './components/NewMessage';
 
-const wsUrl = 'ws://localhost:3001';
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+);
+
+const wsUrl = isLocalhost ? 'ws://localhost:3001' : 'ws://git_chat_room.gititregev.com:3001';
 
 class ChatRoom extends Component {
     state = {
